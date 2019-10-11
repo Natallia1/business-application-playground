500cf1640289ac71683fcc06fea0032d5a0fb824
Natallia1 <natallia.khatsialouskaya@gmail.com>
2019-10-11 23:59:15 +0300

    Added DbQuery class

---
+++ BookStore.BackOffice.WebApi/DbQuery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.BackOffice.WebApi.Models;
+
+namespace BookStore.BackOffice.WebApi
+{
+    public class DbQuery
+    {
+        private readonly BookStoreDbContext context;
+        public DbQuery(BookStoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Book> GetFilteredBooks(Filter filter)
+        {
+            var bookFilters = filter.GetBookFilters();
+            var authorFilters = filter.GetAuthorFilters();
+
+            return context.Books
+                .Where(b => bookFilters(b) && authorFilters(b.Author))
+                .ToList<Book>();
+        }
+    }
+}
