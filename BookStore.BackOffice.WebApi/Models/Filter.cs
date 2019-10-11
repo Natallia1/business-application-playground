673e4f5b47128b7c1193b814cb96f33e7bbc6234
Natallia1 <natallia.khatsialouskaya@gmail.com>
2019-10-11 23:58:44 +0300

    Added filter methods

--- BookStore.BackOffice.WebApi/Models/Filter.cs
+++ BookStore.BackOffice.WebApi/Models/Filter.cs
@@ -1,18 +1,63 @@
-using System.Collections.Generic;
+using System;
+using BookStore.BackOffice.WebApi.Enums;
 
 namespace BookStore.BackOffice.WebApi.Models
 {
     public class Filter
     {
-        public int PublicationYear { get; set; }
-        public Period PeriodRelativeToPublicationYear { get; set; }
-        public bool IsBestSeller { get; set; }
-        public List<Author> Author { get; set; }
-    }
+        public int? PublicationYear { get; set; }
+        public Period? PeriodRelativeToPublicationYear { get; set; }
+        public bool? IsBestSeller { get; set; }
+        public int? AuthorId { get; set; }
 
-    public enum Period
-    {
-        BeforeOrEqual,
-        AfterOrEqual
+        public Func<Author, bool> GetAuthorFilters()
+        {
+            var author = GetAuthorIdFilter();
+            return a => author(a);
+        }
+
+        public Func<Book, bool> GetBookFilters()
+        {
+            var isBestseller = GetIsBestsellerFilter();
+            var period = GetPeriodFilter();
+
+            return b => isBestseller(b) && period(b); 
+        }
+
+        private Func<Book, bool> GetIsBestsellerFilter()
+        {
+            if(IsBestSeller!=null)
+            {
+                return (b => b.IsBestSeller == IsBestSeller);
+            }
+            return null;
+        }
+
+        private Func<Book, bool> GetPeriodFilter()
+        {
+            if (PublicationYear!= null)
+            {
+                switch(PeriodRelativeToPublicationYear)
+                {
+                    case Period.AfterOrEqual:
+                        return b => b.PublicationYear >= PublicationYear;
+                    case Period.BeforeOrEqual:
+                        return b => b.PublicationYear <= PublicationYear;
+                    default:
+                        throw new NotImplementedException
+                            ($"{PeriodRelativeToPublicationYear.ToString()} option is not implemented.");         
+                }
+            }
+            return null;
+        }
+
+        private Func<Author, bool> GetAuthorIdFilter()
+        {
+            if(AuthorId!=null)
+            {
+                return (a => a.Id == AuthorId);
+            }
+            return null;
+        }
     }
 }
